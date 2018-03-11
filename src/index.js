0f6e6fd6d88a2f0d97c3b198e2d4a03007b52359
SotaSuzuki <3.3sota.suzuki@gmail.com>
2018-03-11 13:15:22 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const App = () => (
+  <div>
+    <h1>Hello Parcel!</h1>
+    <p>Zero configuration is really great.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, aliquam et eligendi magni, fugiat harum! Amet reprehenderit ea, distinctio dicta molestias ab minima sed molestiae, officiis explicabo cumque perspiciatis inventore.</p>
+  </div>
+)
+
+render(<App/>, document.querySelector('#app'))
